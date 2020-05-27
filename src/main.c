51686ed1e9712fe9cbfd2c46b3e52220bd140c84
natacha.padova <natacha.padova@epita.fr>
2020-05-28 01:57:17 +0200

    [SHELL] : handled all input kinds

--- src/main.c
+++ src/main.c
@@ -1,10 +1,19 @@
 #define _GNU_SOURCE
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <err.h>
 
-int main()
+#include "parse.h"
+#include "exec.h"
+#include "builtin.h"
+
+void sigintHandler (int sig_num)
+{
+    sig_num = sig_num;
+    write(1, "\nminishell$ ", 12);
+    signal(SIGINT, sigintHandler);
+}
+
+
+void shell_loop(FILE *file)
 {
     size_t input_size = 2048;
     char* input = (char *)calloc(sizeof(char), input_size);
@@ -14,12 +23,56 @@ int main()
         err(12, "calloc error");
     }
     //write prompt
-    write(1, "$>", 3);
-    while (getline(&input, &input_size, stdin) > 0)
+    if (isatty (fileno(file)))
+        write(1, "minishell$ ", 11);
+    signal(SIGINT, sigintHandler);
+    while (getline(&input, &input_size, file) > 0)
     {
-        printf("prout %s\n", input);
-        write(1, "$>", 3);
+        if (input[0] == '\0')
+            continue;
+
+        // handling empty input
+        if (input[0] == '\n')
+        {
+            if (isatty (fileno(file)))
+                write(1, "minishell$ ", 11);
+            continue;
+        }
+        char** cmd = parse(input, " \n\t");
+        if (cmd[0] == NULL)
+            printf("Command not found\n");
+        else if(is_builtin(cmd[0]) == false)
+        {
+            abs_path(cmd);
+            exec(cmd);
+        }
+        else
+            exec_builtin(cmd);
+        if (isatty (fileno(file)))
+            write(1, "minishell$ ", 11);
+        free_array(cmd);
     }
 
     free(input);
+}
+
+
+int main(int argc, char** argv)
+{
+    if (argc != 1)
+    {
+        FILE *file = fopen(argv[1], "r");
+        if (!file)
+        {
+            fprintf(stderr, "minishell: %s: no such file or directory\n", argv[1]);
+            return 127;
+        }
+        else
+        {
+            shell_loop(file);
+            fclose(file);
+        }
+    }
+    else
+        shell_loop(stdin);
 }
